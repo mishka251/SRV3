aa795b86fe4836983a200f60cf76750753e58b11
Mikhail Belov <mbelov@os-company.ru>
2018-11-01 14:45:22 +0500

    Лаба сдана

---
+++ SRVLab3/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SRVLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFromImage = new System.Windows.Forms.ListBox();
+            this.tbToConsole = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tbCommand = new System.Windows.Forms.TextBox();
+            this.lblCommand = new System.Windows.Forms.Label();
+            this.lblTextToConsole = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbFromImage
+            // 
+            this.lbFromImage.FormattingEnabled = true;
+            this.lbFromImage.Location = new System.Drawing.Point(12, 111);
+            this.lbFromImage.Name = "lbFromImage";
+            this.lbFromImage.Size = new System.Drawing.Size(595, 212);
+            this.lbFromImage.TabIndex = 0;
+            // 
+            // tbToConsole
+            // 
+            this.tbToConsole.Location = new System.Drawing.Point(54, 29);
+            this.tbToConsole.Name = "tbToConsole";
+            this.tbToConsole.Size = new System.Drawing.Size(198, 20);
+            this.tbToConsole.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(345, 55);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(91, 24);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Запуск";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // tbCommand
+            // 
+            this.tbCommand.Location = new System.Drawing.Point(345, 29);
+            this.tbCommand.Name = "tbCommand";
+            this.tbCommand.Size = new System.Drawing.Size(198, 20);
+            this.tbCommand.TabIndex = 3;
+            // 
+            // lblCommand
+            // 
+            this.lblCommand.AutoSize = true;
+            this.lblCommand.Location = new System.Drawing.Point(412, 9);
+            this.lblCommand.Name = "lblCommand";
+            this.lblCommand.Size = new System.Drawing.Size(71, 13);
+            this.lblCommand.TabIndex = 4;
+            this.lblCommand.Text = "Приложение";
+            // 
+            // lblTextToConsole
+            // 
+            this.lblTextToConsole.AutoSize = true;
+            this.lblTextToConsole.Location = new System.Drawing.Point(51, 8);
+            this.lblTextToConsole.Name = "lblTextToConsole";
+            this.lblTextToConsole.Size = new System.Drawing.Size(115, 13);
+            this.lblTextToConsole.TabIndex = 5;
+            this.lblTextToConsole.Text = "Сообщение(команда)";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(54, 54);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(79, 25);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Отправка";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 335);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblTextToConsole);
+            this.Controls.Add(this.lblCommand);
+            this.Controls.Add(this.tbCommand);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbToConsole);
+            this.Controls.Add(this.lbFromImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbFromImage;
+        private System.Windows.Forms.TextBox tbToConsole;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tbCommand;
+        private System.Windows.Forms.Label lblCommand;
+        private System.Windows.Forms.Label lblTextToConsole;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ SRVLab3/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+
+namespace SRVLab3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Process cmd;
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            Run();
+        }
+
+        void Send()
+        {
+            try
+            {
+                var pWriter = cmd.StandardInput;
+
+                if (pWriter.BaseStream.CanWrite)
+                {
+                    pWriter.WriteLine(tbToConsole.Text );
+                }
+                else
+                    MessageBox.Show("Cant Write");
+                pWriter.Flush();
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void Run()
+        {
+            if (cmd != null && !cmd.HasExited)
+                cmd.Kill();
+            try
+            {
+                cmd = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = tbCommand.Text,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        StandardOutputEncoding = Encoding.GetEncoding(866),
+                    }
+                };
+                cmd.OutputDataReceived += new DataReceivedEventHandler(Cmd_OutputDataReceived);
+
+                cmd.Start();
+                cmd.BeginOutputReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void Cmd_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+                lbFromImage.Invoke(new Action(() => lbFromImage.Items.Add(e.Data)));
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!cmd.HasExited)
+                cmd.Kill();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+    }
+}
